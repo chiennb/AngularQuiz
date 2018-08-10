3e1a32ce2850ee662a6ca5e18008fe00e80a93bd
ChienNB <chiennb@gmail.com>
2018-08-11 00:19:50 +0700

    02.01 Home

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
 
 import { AppComponent } from './app.component';
+import { QuestionComponent } from './question.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, BrowserAnimationsModule, 
+    MatButtonModule, 
+    MatInputModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
