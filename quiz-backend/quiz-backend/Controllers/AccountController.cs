27be327f2460ac3890859853b58b2c6186f33945
chiennb <chiennb@gmail.com>
2018-08-13 18:10:54 +0700

    Begin 08

---
+++ quiz-backend/quiz-backend/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace quiz_backend.Controllers
+{
+    public class Credentials
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+    [Produces("application/json")]
+    [Route("api/Account")]
+    public class AccountController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody]Credentials credentials)
+        {
+            var jwt = new JwtSecurityToken();
+            return Ok(new JwtSecurityTokenHandler().WriteToken(jwt));
+        }
+    }
+}
